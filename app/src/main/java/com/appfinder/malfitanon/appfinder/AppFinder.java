25aee45b3f62454dda18d44966ec648471cdf87a
Nicholas Malfitano <nicholas2malfitano@gmail.com>
2016-09-21 13:23:08 -0700

    project just setup

---
+++ app/src/main/java/com/appfinder/malfitanon/appfinder/AppFinder.java
@@ -0,0 +1,13 @@
+package com.appfinder.malfitanon.appfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppFinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_finder);
+    }
+}
