6a4cc9f4b71109af7899b5e16b8ffe1a8bdc125e
Nicholas Malfitano <nicholas2malfitano@gmail.com>
2016-09-23 13:42:46 -0700

    real project commit 2

--- app/src/main/java/com/appfinder/malfitanon/appfinder/AppFinder.java
+++ app/src/main/java/com/appfinder/malfitanon/appfinder/AppFinder.java
@@ -2,12 +2,18 @@ package com.appfinder.malfitanon.appfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AppFinder extends AppCompatActivity {
+
+    private TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_finder);
+
+        answerText = (TextView) findViewById(R.id.answerText);
+        answerText.setText("No, You Suck");
     }
 }
---
+++ app/src/main/java/com/appfinder/malfitanon/appfinder/Predictions.java
@@ -0,0 +1,7 @@
+package com.appfinder.malfitanon.appfinder;
+
+/**
+ * Created by Student on 9/22/2016.
+ */
+public class Predictions {
+}
